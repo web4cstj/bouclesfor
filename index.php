1dcfdc50fede1b87abe7b479622bafc29ad9ab53
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2021-01-01 22:25:29 -0500

    Départ

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
 /**********************************************************************************
- ____  ____  ___    ____  _____  __  __  ___  __    ____  ___    ____  _____  ____ 
+ ____  ____  ___    ____  _____  __  __  ___  __    ____  ___    ____  _____  ____
 (  _ \( ___)/ __)  (  _ \(  _  )(  )(  )/ __)(  )  ( ___)/ __)  ( ___)(  _  )(  _ \
  )(_) ))__) \__ \   ) _ < )(_)(  )(__)(( (__  )(__  )__) \__ \   )__)  )(_)(  )   /
 (____/(____)(___/  (____/(_____)(______)\___)(____)(____)(___/  (__)  (_____)(_)\_)
 ===================================================================================
 Suivre les indications dans la page
-Chaque affichage doit se faire :
-  - soit avec des echos successifs
-  - soit avec des concaténations successives
-  - soit avec une fonction
-Dans ce solutionnaire, nous vous offrons les 3 pour chaque affichage. La "vraie" 
-solution est celle qui n'est pas en commentaire.
 ***********************************************************************************/
 ?><!DOCTYPE html>
 <html lang="fr">
@@ -21,239 +15,380 @@ solution est celle qui n'est pas en commentaire.
 </head>
 
 <body>
+<h1>Multiplication verticale</h1>
 <?php
-echo '<h1>Une table de multiplication</h1>';
 /********************************************************************************
 - Rendre dynamique le tableau suivant en utilisant une boucle for
-- La page DOIT être valide au W3C
-- Utilisez la méthode des "echo" successifs
+- Utiliser la méthode des "echo" successifs
 ********************************************************************************/
-/* SOLUTION */
-// Avec des echos successifs
-$mult = 12;
-echo '<table border="1">';
-echo '<tr><th>&times;</th><th>'.$mult.'</th></tr>';
-for ($x=1; $x<=12; $x++){
-	echo '<tr><th>'.$x.'</th><td>'.($x*$mult).'</td></tr>';
-}
-echo '</table>';
 
-// Avec des concaténations successives
-/*
-$mult = 12;
-$affichage = '<table border="1">';
-$affichage .= '<tr><th>&times;</th><th>'.$mult.'</th></tr>';
-for ($x=1; $x<=12; $x++){
-	$affichage .= '<tr><th>'.$x.'</th><td>'.($x*$mult).'</td></tr>';
-}
-$affichage .= '</table>';
-echo $affichage;
-*/
+?>
+<table border="1">
+	<tr>
+		<th>×</th>
+		<th>12</th>
+	</tr>
+	<tr>
+		<th>1</th>
+		<td>12</td>
+	</tr>
+	<tr>
+		<th>2</th>
+		<td>24</td>
+	</tr>
+	<tr>
+		<th>3</th>
+		<td>36</td>
+	</tr>
+	<tr>
+		<th>4</th>
+		<td>48</td>
+	</tr>
+	<tr>
+		<th>5</th>
+		<td>60</td>
+	</tr>
+	<tr>
+		<th>6</th>
+		<td>72</td>
+	</tr>
+	<tr>
+		<th>7</th>
+		<td>84</td>
+	</tr>
+	<tr>
+		<th>8</th>
+		<td>96</td>
+	</tr>
+	<tr>
+		<th>9</th>
+		<td>108</td>
+	</tr>
+	<tr>
+		<th>10</th>
+		<td>120</td>
+	</tr>
+	<tr>
+		<th>11</th>
+		<td>132</td>
+	</tr>
+	<tr>
+		<th>12</th>
+		<td>144</td>
+	</tr>
+</table>
 
-// À l'aide d'une fonction
-/*
-function table1($mult) {
-  $affichage = '<table border="1">';
-  $affichage .= '<tr><th>&times;</th><th>'.$mult.'</th></tr>';
-  for ($x=1; $x<=12; $x++){
-    $affichage .= '<tr><th>'.$x.'</th><td>'.($x*$mult).'</td></tr>';
-  }
-  $affichage .= '</table>';
-  return $affichage;
-}
-echo table1(12);
-*/
-/* /SOLUTION */
-
-echo '<h1>Une autre table de multiplication</h1>';
-
+<h1>Multiplication horizontale</h1>
+<?php
 /********************************************************************************
 - Rendre dynamique le tableau suivant en utilisant des boucles for
-- La page DOIT être valide au W3C
-- Utilisez la méthode des concaténations successives dans une variable 
+- Utiliser la méthode des concaténations successives dans une variable
   avec affichage à la fin
+- Note: Il n'y a pas de boucles imbriquées
 ********************************************************************************/
-/* SOLUTION */
-// Avec des echos successifs
-/*$mult = 8;
-echo '<table border="1">';
-echo '<tr><th>&times;</th>';
-for ($x=1; $x<=12; $x++){
-	echo '<th>'.$x.'</th>';
-}
-echo '</tr><tr><th>'.$mult.'</th>';
-for ($x=1; $x<=12; $x++){
-	echo '<td>'.($x*$mult).'</td>';
-}
-echo '</tr>';
-echo '</table>';
-*/
 
-// Avec des concaténations successives
-$mult = 8;
-$affichage = '<table border="1">';
-$affichage .= '<tr><th>&times;</th>';
-for ($x=1; $x<=12; $x++){
-	$affichage .= '<th>'.$x.'</th>';
-}
-$affichage .= '</tr><tr><th>'.$mult.'</th>';
-for ($x=1; $x<=12; $x++){
-	$affichage .= '<td>'.($x*$mult).'</td>';
-}
-$affichage .= '</tr>';
-$affichage .= '</table>';
-echo $affichage;
+?>
+<table border="1">
+	<tr>
+		<th>&times;</th>
+		<th>1</th>
+		<th>2</th>
+		<th>3</th>
+		<th>4</th>
+		<th>5</th>
+		<th>6</th>
+		<th>7</th>
+		<th>8</th>
+		<th>9</th>
+		<th>10</th>
+		<th>11</th>
+		<th>12</th>
+	</tr>
+	<tr>
+		<th>8</th>
+		<td>8</td>
+		<td>16</td>
+		<td>24</td>
+		<td>32</td>
+		<td>40</td>
+		<td>48</td>
+		<td>56</td>
+		<td>64</td>
+		<td>72</td>
+		<td>80</td>
+		<td>88</td>
+		<td>96</td>
+	</tr>
+</table>
 
-// À l'aide d'une fonction 
-/*
-function table2($mult) {
-  $affichage = '<table border="1">';
-  $affichage .= '<tr><th>&times;</th>';
-  for ($x=1; $x<=12; $x++){
-    $affichage .= '<th>'.$x.'</th>';
-  }
-  $affichage .= '</tr><tr><th>'.$mult.'</th>';
-  for ($x=1; $x<=12; $x++){
-    $affichage .= '<td>'.($x*$mult).'</td>';
-  }
-  $affichage .= '</tr>';
-  $affichage .= '</table>';
-  return $affichage;
-}
-echo table2(8);
-*/
-/* /SOLUTION */
-
-echo '<h1>Des tables de multiplication</h1>';
-
+<h1>Table de multiplications</h1>
+<?php
 /********************************************************************************
 - Rendre dynamique le tableau suivant en utilisant des boucles for imbriquées
-- La page DOIT être valide au W3C
-- Utilisez une fonction qui retourne le code 
+- Utilisez une fonction qui retourne le code
   avec affichage de la valeur de retour
 ********************************************************************************/
-/* SOLUTION */
-// Avec des echos successifs
-/*
-echo '<table border="1">';
-echo '<tr><th>&times;</th>';
-for ($x=1; $x<=12; $x++){
-	echo '<th>'.$x.'</th>';
-}
-echo '</tr>';
-for ($x=1; $x<=12; $x++){
-	echo '<tr><th>'.$x.'</th>';
-	for ($mult=1; $mult<=12; $mult++){
-		echo '<td>'.($x*$mult).'</td>';
-	}
-	echo '</tr>';
-}
-echo '</table>';
-*/
 
-// Avec des concaténations successives
-/*
-$affichage = '<table border="1">';
-$affichage .= '<tr><th>&times;</th>';
-for ($x=1; $x<=12; $x++){
-	$affichage .= '<th>'.$x.'</th>';
-}
-$affichage .= '</tr>';
-for ($x=1; $x<=12; $x++){
-	$affichage .= '<tr><th>'.$x.'</th>';
-	for ($mult=1; $mult<=12; $mult++){
-		$affichage .= '<td>'.($x*$mult).'</td>';
-	}
-	$affichage .= '</tr>';
-}
-$affichage .= '</table>';
-echo $affichage;
-*/
+?>
+<table border="1">
+	<tr>
+		<th>&times;</th>
+		<th>1</th>
+		<th>2</th>
+		<th>3</th>
+		<th>4</th>
+		<th>5</th>
+		<th>6</th>
+		<th>7</th>
+		<th>8</th>
+		<th>9</th>
+		<th>10</th>
+		<th>11</th>
+		<th>12</th>
+	</tr>
+	<tr>
+		<th>1</th>
+		<td>1</td>
+		<td>2</td>
+		<td>3</td>
+		<td>4</td>
+		<td>5</td>
+		<td>6</td>
+		<td>7</td>
+		<td>8</td>
+		<td>9</td>
+		<td>10</td>
+		<td>11</td>
+		<td>12</td>
+	</tr>
+	<tr>
+		<th>2</th>
+		<td>2</td>
+		<td>4</td>
+		<td>6</td>
+		<td>8</td>
+		<td>10</td>
+		<td>12</td>
+		<td>14</td>
+		<td>16</td>
+		<td>18</td>
+		<td>20</td>
+		<td>22</td>
+		<td>24</td>
+	</tr>
+	<tr>
+		<th>3</th>
+		<td>3</td>
+		<td>6</td>
+		<td>9</td>
+		<td>12</td>
+		<td>15</td>
+		<td>18</td>
+		<td>21</td>
+		<td>24</td>
+		<td>27</td>
+		<td>30</td>
+		<td>33</td>
+		<td>36</td>
+	</tr>
+	<tr>
+		<th>4</th>
+		<td>4</td>
+		<td>8</td>
+		<td>12</td>
+		<td>16</td>
+		<td>20</td>
+		<td>24</td>
+		<td>28</td>
+		<td>32</td>
+		<td>36</td>
+		<td>40</td>
+		<td>44</td>
+		<td>48</td>
+	</tr>
+	<tr>
+		<th>5</th>
+		<td>5</td>
+		<td>10</td>
+		<td>15</td>
+		<td>20</td>
+		<td>25</td>
+		<td>30</td>
+		<td>35</td>
+		<td>40</td>
+		<td>45</td>
+		<td>50</td>
+		<td>55</td>
+		<td>60</td>
+	</tr>
+	<tr>
+		<th>6</th>
+		<td>6</td>
+		<td>12</td>
+		<td>18</td>
+		<td>24</td>
+		<td>30</td>
+		<td>36</td>
+		<td>42</td>
+		<td>48</td>
+		<td>54</td>
+		<td>60</td>
+		<td>66</td>
+		<td>72</td>
+	</tr>
+	<tr>
+		<th>7</th>
+		<td>7</td>
+		<td>14</td>
+		<td>21</td>
+		<td>28</td>
+		<td>35</td>
+		<td>42</td>
+		<td>49</td>
+		<td>56</td>
+		<td>63</td>
+		<td>70</td>
+		<td>77</td>
+		<td>84</td>
+	</tr>
+	<tr>
+		<th>8</th>
+		<td>8</td>
+		<td>16</td>
+		<td>24</td>
+		<td>32</td>
+		<td>40</td>
+		<td>48</td>
+		<td>56</td>
+		<td>64</td>
+		<td>72</td>
+		<td>80</td>
+		<td>88</td>
+		<td>96</td>
+	</tr>
+	<tr>
+		<th>9</th>
+		<td>9</td>
+		<td>18</td>
+		<td>27</td>
+		<td>36</td>
+		<td>45</td>
+		<td>54</td>
+		<td>63</td>
+		<td>72</td>
+		<td>81</td>
+		<td>90</td>
+		<td>99</td>
+		<td>108</td>
+	</tr>
+	<tr>
+		<th>10</th>
+		<td>10</td>
+		<td>20</td>
+		<td>30</td>
+		<td>40</td>
+		<td>50</td>
+		<td>60</td>
+		<td>70</td>
+		<td>80</td>
+		<td>90</td>
+		<td>100</td>
+		<td>110</td>
+		<td>120</td>
+	</tr>
+	<tr>
+		<th>11</th>
+		<td>11</td>
+		<td>22</td>
+		<td>33</td>
+		<td>44</td>
+		<td>55</td>
+		<td>66</td>
+		<td>77</td>
+		<td>88</td>
+		<td>99</td>
+		<td>110</td>
+		<td>121</td>
+		<td>132</td>
+	</tr>
+	<tr>
+		<th>12</th>
+		<td>12</td>
+		<td>24</td>
+		<td>36</td>
+		<td>48</td>
+		<td>60</td>
+		<td>72</td>
+		<td>84</td>
+		<td>96</td>
+		<td>108</td>
+		<td>120</td>
+		<td>132</td>
+		<td>144</td>
+	</tr>
+</table>
 
-// À l'aide d'une fonction 
-function tables2d() {
-    $affichage = '<table border="1">';
-    $affichage .= '<tr><th>&times;</th>';
-    for ($x=1; $x<=12; $x++){
-        $affichage .= '<th>'.$x.'</th>';
-    }
-    $affichage .= '</tr>';
-    for ($x=1; $x<=12; $x++){
-        $affichage .= '<tr><th>'.$x.'</th>';
-        for ($mult=1; $mult<=12; $mult++){
-            $affichage .= '<td>'.($x*$mult).'</td>';
-        }
-        $affichage .= '</tr>';
-    }
-    $affichage .= '</table>';
-    return $affichage;
-}
-echo tables2d();
-/* /SOLUTION */
-
-echo '<h1>La somme des nombres</h1>';
-
+<h1>Bonus : La somme des nombres</h1>
+<?php
 /********************************************************************************
 - Rendre dynamique le tableau suivant en utilisant une boucle for
-- La page DOIT être valide au W3C
 - Utilisez la méthode que vous voulez
 ********************************************************************************/
-/* SOLUTION */
-// Avec des echos successifs
-/*
-$total = 0;
-echo '<table border="1">';
-echo '<tr><th>&sum;</th><th>&nbsp;</th></tr>';
-for ($x=1; $x<=12; $x++){
-	$total += $x;
-	echo '<tr><th>'.$x.'</th><td>'.$total.'</td></tr>';
-}
-echo '</table>';
-*/
 
-// Avec des concaténations successives
-/*
-$total = 0;
-$affichage = '<table border="1">';
-$affichage .= '<tr><th>&sum;</th><th>&nbsp;</th></tr>';
-for ($x=1; $x<=12; $x++){
-	$total += $x;
-	$affichage .= '<tr><th>'.$x.'</th><td>'.$total.'</td></tr>';
-}
-$affichage .= '</table>';
-echo $affichage;
-*/
-
-// À l'aide d'une fonction 
-function sommeDeNombres() {
-    $total = 0;
-    $affichage = '<table border="1">';
-    $affichage .= '<tr><th>&sum;</th><th>&nbsp;</th></tr>';
-    for ($x=1; $x<=12; $x++){
-        $total += $x;
-        $affichage .= '<tr><th>'.$x.'</th><td>'.$total.'</td></tr>';
-    }
-    $affichage .= '</table>';
-    return $affichage;
-}
-echo sommeDeNombres();
-
-// Une méthode statique dans une classe
-class Boucle {
-    function sommeDeNombres() {
-        $total = 0;
-        $affichage = '<table border="1">';
-        $affichage .= '<tr><th>&sum;</th><th>&nbsp;</th></tr>';
-        for ($x=1; $x<=12; $x++){
-            $total += $x;
-            $affichage .= '<tr><th>'.$x.'</th><td>'.$total.'</td></tr>';
-        }
-        $affichage .= '</table>';
-        return $affichage;
-    }
-}
-echo sommeDeNombres();
-/* /SOLUTION */
 ?>
+<table border="1">
+	<tr>
+		<th>&sum;</th>
+		<th>&nbsp;</th>
+	</tr>
+	<tr>
+		<th>1</th>
+		<td>1</td>
+	</tr>
+	<tr>
+		<th>2</th>
+		<td>3</td>
+	</tr>
+	<tr>
+		<th>3</th>
+		<td>6</td>
+	</tr>
+	<tr>
+		<th>4</th>
+		<td>10</td>
+	</tr>
+	<tr>
+		<th>5</th>
+		<td>15</td>
+	</tr>
+	<tr>
+		<th>6</th>
+		<td>21</td>
+	</tr>
+	<tr>
+		<th>7</th>
+		<td>28</td>
+	</tr>
+	<tr>
+		<th>8</th>
+		<td>36</td>
+	</tr>
+	<tr>
+		<th>9</th>
+		<td>45</td>
+	</tr>
+	<tr>
+		<th>10</th>
+		<td>55</td>
+	</tr>
+	<tr>
+		<th>11</th>
+		<td>66</td>
+	</tr>
+	<tr>
+		<th>12</th>
+		<td>78</td>
+	</tr>
+</table>
 </body>
 </html>
